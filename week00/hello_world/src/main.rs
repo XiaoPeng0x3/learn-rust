534351b9a3449ac829fc9451df3900aef3ad4242
XiaoPeng <zxpcs0x3@gmail.com>
2025-07-17 22:24:17 +0800

    learn:第一个rust程序！

---
+++ week00/hello_world/src/main.rs
@@ -0,0 +1,10 @@
+/**
+ *  简单的输入输出
+ *  这个定义比较奇怪，而且在末尾还有一个！
+ */
+
+fn main() {
+    println!("Hello, world!");
+    println!("I am a bit rusty with this new language!");
+    println!("This program is 🚀 blazingly fast 🚀");
+}
